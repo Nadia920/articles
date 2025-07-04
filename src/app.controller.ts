7a5d7c27f8ac005f68c44e9c8113c3d01ad2b36d
Nadia920 <kabaknada64@gmail.com>
2025-07-04 17:00:32 +0300

    Initial state

---
+++ src/app.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Render, Param, ParseIntPipe, Body, Post, Redirect } from '@nestjs/common';
+import { articles } from './articles';
+import { Article } from './article.model';
+
+@Controller()
+export class AppController {
+   
+  @Get('create')
+  @Render('create-article')
+  getForm(): void {
+    return;
+  }
+
+  @Post('articles')
+  @Redirect('/', 301)
+  create(@Body() body: any): void {
+    const id = articles.length + 1;
+    const article = new Article(body.title, body.content, id);
+    articles.push(article);
+  }
+
+  @Get()
+  @Render('index')
+  index() {
+    return { articles };
+  }
+
+  @Get(':id')
+  @Render('article')
+  getById(@Param('id', ParseIntPipe) id: number) {
+    return articles.find(article => article.id === id);
+  }
+}
